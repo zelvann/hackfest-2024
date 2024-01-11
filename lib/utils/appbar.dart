8c1267cbf578fdcbea2e5ffe9e0cffd8b27309ec
zelvann <zelvan1701@gmail.com>
2024-01-11 10:44:39 +0700

    make an integrate between src and add firebase dep

--- lib/utils/appbar.dart
+++ lib/utils/appbar.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-void main() => runApp(const Appbartampan());
 
-class Appbartampan extends StatelessWidget {
+class Appbartampan extends StatelessWidget implements PreferredSizeWidget {
   const Appbartampan({super.key});
 
   @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Image.asset('images/logo.png',height: 34,width: 34),
-              const SizedBox(width: 9),
-              Text(
-                'TAMPAN',
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontFamily: 'Poppins',
-                  color: HexColor("#023047"),
-                ),
-              )
-            ],
+    return AppBar(
+      title: Row(
+        children: [
+          Image.asset(
+            'images/logo.png',
+            height: 34,
+            width: 34,
           ),
-        ),
+          const SizedBox(width: 9),
+          Text(
+            'TAMPAN',
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Poppins',
+              color: HexColor("#023047"),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/utils/bottomnav.dart
+++ lib/utils/bottomnav.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-void main() => runApp(const Botnavtampan());
-
-class Botnavtampan extends StatelessWidget {
-  const Botnavtampan ({super.key});
+class Botnav extends StatefulWidget implements PreferredSizeWidget  {
+  const Botnav({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const Botnav(),
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-      ),
-    );
-  }
-}
-
-class Botnav extends StatefulWidget {
-  const Botnav({super.key});
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   State<Botnav> createState() => _BotnavState();
@@ -29,52 +16,50 @@ class _BotnavState extends State<Botnav> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (index) {
-          setState(() {
-            cur = index;
-          });
+    return NavigationBar(
+      onDestinationSelected: (index) {
+        setState(() {
+          cur = index;
+        });
         },
-        backgroundColor: HexColor("#EAF8FF"),
-        indicatorColor: Colors.black,
-        selectedIndex: cur,
-        destinations: const <Widget> [
-          NavigationDestination(
-              selectedIcon: Icon(
-                Icons.home,
-                color: Colors.white
-              ),
-              icon: Icon(
-                Icons.home,
-                color: Colors.black
-              ),
-              label: "Home",
+      backgroundColor: HexColor("#EAF8FF"),
+      indicatorColor: Colors.black,
+      selectedIndex: cur,
+      destinations: const <Widget> [
+        NavigationDestination(
+          selectedIcon: Icon(
+              Icons.home,
+              color: Colors.white
           ),
-          NavigationDestination(
-              selectedIcon: Icon(
-                Icons.comment_bank_rounded,
-                color: Colors.white,
-              ),
-              icon: Icon(
-                Icons.comment_bank_rounded,
-                color: Colors.black,
-              ),
-              label: "Komunitas",
+          icon: Icon(
+              Icons.home,
+              color: Colors.black
           ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.accessibility_new,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.accessibility_new,
-              color: Colors.black,
-            ),
-            label: "Profile",
+          label: "Home",
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(
+            Icons.comment_bank_rounded,
+            color: Colors.white,
           ),
-        ],
-      ),
+          icon: Icon(
+            Icons.comment_bank_rounded,
+            color: Colors.black,
+          ),
+          label: "Komunitas",
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(
+            Icons.accessibility_new,
+            color: Colors.white,
+          ),
+          icon: Icon(
+            Icons.accessibility_new,
+            color: Colors.black,
+          ),
+          label: "Profile",
+        ),
+      ],
     );
   }
 }
