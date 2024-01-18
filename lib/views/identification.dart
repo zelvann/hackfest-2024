1da36f10dc592c5ac74f18665b9eb05b8beab9a3
zelvann <zelvan1701@gmail.com>
2024-01-18 20:58:07 +0700

    make an input for picking image from gallery

--- lib/views/identification.dart
+++ lib/views/identification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tampan/utils/navigator.dart';
 import 'package:tampan/views/result.dart';
 
@@ -9,77 +10,91 @@ class Ipg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-                margin: const EdgeInsets.only(bottom: 30),
-                child: Text(
-                    'PILIH OPSI GAMBAR',
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: HexColor('#023047'),
-                        fontWeight: FontWeight.w700
-                    )
-                )
-            ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context)
-                            => Nextpg(
-                                appbar:AppBar(
-                                    title: Text(
-                                        'IDENTIFIKASI IKAN',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w800,
-                                            color: HexColor('#023047')
-                                        )
-                                    )
-                                ),
-                                body: const Respg()
-                            ))
-                        );
-                      },
-                      icon: Image.asset(
-                        'images/icon/camera.png',
-                        width: 115, height: 158
-                      )
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context)
-                            => Nextpg(
-                                appbar: AppBar(
-                                    title: Text(
-                                        'IDENTIFIKASI IKAN',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w800,
-                                            color: HexColor('#023047')
-                                        )
-                                    )
-                                ),
-                                body: const Respg()
-                            ))
-                        );
-                      },
-                      icon: Image.asset(
-                          'images/icon/gallery.png',
-                          width: 115, height: 158
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  margin: const EdgeInsets.only(bottom: 30),
+                  child: Text('PILIH OPSI GAMBAR',
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: HexColor('#023047'),
+                          fontWeight: FontWeight.w700
                       )
                   )
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                    onPressed: () async {
+                      final XFile? pickedImage = await ImagePicker()
+                          .pickImage(source: ImageSource.camera);
+                      if (pickedImage != null) {
+                        if(context.mounted) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context)
+                              => Nextpg(
+                                appbar: AppBar(
+                                  title: Text(
+                                    'IDENTIFIKASI IKAN',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      color: HexColor('#023047')
+                                    )
+                                  )
+                                ),
+                                body: Respg(image: pickedImage)
+                              )
+                            )
+                          );
+                        }
+                      }
+                    },
+                    icon: Image.asset(
+                        'images/icon/camera.png',
+                        width: 115, height: 158)
+                    ),
+                IconButton(
+                    onPressed: () async {
+                      final XFile? pickedImage = await ImagePicker()
+                          .pickImage(source: ImageSource.gallery);
+                      if (pickedImage != null) {
+                        if(context.mounted) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context)
+                                => Nextpg(
+                                    appbar: AppBar(
+                                        title: Text(
+                                            'IDENTIFIKASI IKAN',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w800,
+                                              color: HexColor('#023047')
+                                            )
+                                        )
+                                    ),
+                                    body: Respg(image: pickedImage),
+                                )
+                              )
+                          );
+                        }
+                      }
+                    },
+                    icon: Image.asset(
+                        'images/icon/gallery.png',
+                        width: 115, height: 158
+                    )
+                  )
                 ]
-            )
-          ]
-      )
+              )
+            ]
+        )
     );
   }
 }
--- lib/views/result.dart
+++ lib/views/result.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Respg extends StatelessWidget {
-  const Respg({super.key});
-
+  const Respg({
+    Key? key,
+    required this.image
+  }) : super(key: key);
+  
+  final XFile image;
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,85 +22,86 @@ class Respg extends StatelessWidget {
           Text(
             'GAMBAR IKAN',
             style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w700,
-              color: HexColor('#023047')
-            ),
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+                color: HexColor('#023047')
+            )
           ),
           Container(
-            margin: const EdgeInsets.only(bottom: 60,top: 10),
-            child: Image.asset(
-              'images/icon/res.png',
-              width: 310, height: 151
-            )
+              margin: const EdgeInsets.only(bottom: 60, top: 10),
+              child: Image.file(
+                      File(image.path),
+                      width: 310, height: 120
+                    )
           ),
           Text(
-            'DIAGNOSIS',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w700,
-              color: HexColor('#023047'),
-            )
+              'DIAGNOSIS',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+                color: HexColor('#023047')
+              )
           ),
           Container(
-            width: double.infinity, height: 260,
-            margin: const EdgeInsets.only(top: 10),
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: HexColor('#EAF8FF'),
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(width: 1,color: Colors.black)
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                    'KONDISI IKAN:',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: HexColor('#8C8C8C'),
-                        fontWeight: FontWeight.w800
-                    )
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  child: const Text(
-                    'SAKIT',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 52,
-                      color: Colors.black
-                    )
-                  )
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+              width: double.infinity,
+              height: 260,
+              margin: const EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: HexColor('#EAF8FF'),
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(width: 1, color: Colors.black)
+              ),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      margin: const EdgeInsets.only(right: 20),
-                      child: Text(
-                          'KETERANGAN :',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w800,
-                              color: HexColor('#8C8C8C')
-                          )
-                      )
-                    ),
-                    Flexible(
-                        child: Text(
-                          'TERJANGKIT VIRUS',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w800,
-                              fontSize: 24,
-                              color: HexColor('#FB8500')
-                          )
+                    Text(
+                        'KONDISI IKAN:',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: HexColor('#8C8C8C'),
+                            fontWeight: FontWeight.w800
                         )
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        child: const Text(
+                            'SAKIT',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 52,
+                                color: Colors.black
+                            )
+                        )
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(right: 20),
+                            child: Text(
+                                'KETERANGAN :',
+                                style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w800,
+                                        color: HexColor('#8C8C8C')
+                                )
+                            )
+                          ),
+                          Flexible(
+                            child: Text(
+                                'TERJANGKIT BAKTERI',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 24,
+                                  color: HexColor('#FB8500')
+                                )
+                            )
+                          )
+                        ]
                     )
                   ]
-                )
-              ]
-            )
+              )
           )
         ]
       )
